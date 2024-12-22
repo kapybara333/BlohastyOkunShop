f6a91a289c84ed6be97dc55c6d247d5ef69e8901
igoyrock1 <Google@gmail.com>
2024-12-22 17:15:04 +0200

    sdfdfdfs

---
+++ admin.js
@@ -0,0 +1,23 @@
+let productForm = document.getElementById('add_product_form');
+productForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let data = JSON.stringify({
+        "name": event.target['name'].value,
+        "description": event.target['description'].value,
+        "price": event.target['price'].value,
+        "photo_url": event.target['photo_url'].value
+    });
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+        console.log(this.responseText);
+    }
+    });
+    xhr.open("POST", "https://kapybarashop-6f07.restdb.io/rest");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "67682325e70533a4333584e7");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.send(data);
+})
+
